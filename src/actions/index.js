188cdf3e79b7602008926652d5a3377ce4a4123a
stefaleon <stefaleon@gmail.com>
2019-11-29 20:59:31 +0200

    map state to props in PostList

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,6 @@ import jsonPlaceholder from "../api/jsonPlaceholder";
 
 export const fetchPosts = () => async dispatch => {
   const res = await jsonPlaceholder.get("/posts");
-  console.log(res.data);
+
   dispatch({ type: FETCH_POSTS, payload: res.data });
 };
