05ce7591118f7fbc338aabdf6673bd1f93b09940
unknown <stefaleon@gmail.com>
2019-11-29 13:10:33 +0200

    setup redux store

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import reducers from "./reducers";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">Ohai!</div>;
+  return (
+    <Provider store={createStore(reducers)}>
+      <div className="App">Ohai!</div>
+    </Provider>
+  );
 }
 
 export default App;
